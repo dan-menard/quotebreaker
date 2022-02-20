3451d1e9229df04b0550194e9ea09779b7079811
Dan Menard <daniel.menard@gmail.com>
2022-02-20 12:34:48 -0500

    Fix a bug where sometimes we'd shuffle a letter into the correct position by mistake. It's uh, elegant, but not especially efficient. Fine for how often it happens, tho.

--- src/util/generateKey.js
+++ src/util/generateKey.js
@@ -1,19 +1,34 @@
+const ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
+
+function validate(key) {
+  const correctLetters = ALPHABET.filter((letter) => {
+    return key[letter] === letter;
+  });
+
+  return !Boolean(correctLetters.length);
+}
+
 export default function generateKey() {
-  const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
-  const scrambled = letters.slice();
+  const letters = ALPHABET.slice();
 
   // Fisher-Yates shuffle:
   // https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
   for (let i=0; i<26; i++) {
     const j = Math.floor(Math.random() * 26);
 
-    const temp = scrambled[i];
-    scrambled[i] = scrambled[j];
-    scrambled[j] = temp;
+    const temp = letters[i];
+    letters[i] = letters[j];
+    letters[j] = temp;
   }
 
-  return letters.reduce((accumulator, letter) => {
-    accumulator[letter] = scrambled.pop();
+  const key = ALPHABET.reduce((accumulator, letter) => {
+    accumulator[letter] = letters.pop();
     return accumulator;
   }, {});
+
+  if (validate(key)) {
+    return key;
+  } else {
+    return generateKey();
+  }
 }
