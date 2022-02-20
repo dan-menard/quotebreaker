24bb037200b9522a27ae3af33c541756b7d0e45c
Dan Menard <daniel.menard@gmail.com>
2022-02-19 22:15:57 -0500

    Ok so it's not pretty, but the letters are all buttons.

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,39 @@
 <script>
-  export let name;
+  export let quote;
+
+  const {author, text} = quote;
+
+  function parseIntoWordsAndLetters(string) {
+    const words = string.split(' ');
+    const wordsAndLetters = words.map((word) => word.split(''));
+
+    return wordsAndLetters;
+  }
+
+  function isLetter(character) {
+    return /\p{L}/u.test(character)
+  }
+
+  const words = parseIntoWordsAndLetters(text);
+  const names = parseIntoWordsAndLetters(author);
 </script>
 
 <main>
-  <h1>Hello {name}!</h1>
-  <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  {#each words as word}
+    <span class="word">
+      {#each word as character}
+        {#if isLetter(character)}
+          <button>{character}</button>
+        {:else}
+          {character}
+        {/if}
+      {/each}
+    </span>
+  {/each}
 </main>
 
 <style>
+  .word {
+    padding: 10px 5px;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ import App from './App.svelte';
 const app = new App({
   target: document.body,
   props: {
-    name: 'world'
+    quote: {
+      text: "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+      author: "Martin Fowler"
+    }
   }
 });
 
